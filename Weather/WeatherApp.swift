39c36d7e8fd104fdb6e73d43ab8a25dc455b9d30
Jagan Kumar Mudila <69246481+mudilajaganios@users.noreply.github.com>
2024-08-16 06:13:33 -0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Jagan Kumar Mudila on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
